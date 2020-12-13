072e2c466e88e1fc395905c0cee1c0e2eb292c2a
daniellerguidini <33754064+daniellerguidini@users.noreply.github.com>
2020-12-12 21:19:05 -0300

    Update spark_R-config.sh

--- spark_R-config.sh
+++ spark_R-config.sh
@@ -8,15 +8,5 @@ export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 # Execução local
 $SPARK_HOME/bin/run-example --master local SparkPi 10
 
-# download R
-sudo apt-get install r-base r-base-core
 
-# instalacao dependencias do pacote sparklyr
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y libicu-dev
-sudo apt-get install -y libxml2-dev
-
-# instalacao dependencias do pacote shiny
-sudo apt-get install -y make
 
