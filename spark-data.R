ea351081f7da3c53ede87d53d39ab43c78658477
daniellerguidini <33754064+daniellerguidini@users.noreply.github.com>
2020-12-17 13:43:30 -0300

    Update spark-data.R

--- spark-data.R
+++ spark-data.R
@@ -1,15 +1,13 @@
 library(sparklyr)
 library(dplyr)
 library(ggplot2)
-spark_install(version = "1.6.2")
+# spark_install(version = "1.6.2")
 options("scipen"=100, "digits"=8)
 sc <- spark_connect(master = "local")
-
 
 data_sp <- spark_read_csv(sc,
                           path = 'E:/Dropbox/Aula/Big Data/US_Accidents_June20.csv')
 
-#numero de acidentes por cidade
 city <- data_sp %>%
   group_by(City) %>%
   summarise(count = n()) %>%
@@ -18,12 +16,11 @@ city <- data_sp %>%
 
 ggplot(city %>% head() , aes(x = reorder(City, -count), y = count, fill = City)) +
   geom_col() +
-  labs(title = 'Cities with more accidents',
+  labs(title = 'Accidents by Cities',
        x = 'Cities',
        y = 'Count') +
   theme_minimal()
 
-#numero de acidentes por condicoes climaticas
 weather <- data_sp %>%
   group_by(Weather_Condition) %>%
   summarise(count = n()) %>%
@@ -32,14 +29,21 @@ weather <- data_sp %>%
 
 ggplot(weather %>% head() , aes(x = reorder(Weather_Condition, -count), y = count, fill = Weather_Condition)) +
   geom_col() +
-  labs(title = 'Weather Conditions with more accidents',
+  labs(title = 'Accidents by Weather Conditions',
        x = 'Weather Conditions',
        y = 'Counts') +
   theme_minimal()
   
-weather <- data_sp %>%
-  group_by(Weather_Condition) %>%
+wind <- data_sp %>%
+  group_by(Wind_Direction) %>%
   summarise(count = n()) %>%
   arrange(-count) %>%
   collect()
+
+ggplot(wind %>% head() , aes(x = reorder(Wind_Direction, -count), y = count, fill = Wind_Direction)) +
+  geom_col() +
+  labs(title = 'Accidents by Wind Direction',
+       x = 'Wind Direction',
+       y = 'Counts') +
+  theme_minimal()
 
